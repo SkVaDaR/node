c2d403df99e0971fbfa324d580a94613fcefc9bf
Sviatoslav Myskiv <79469781+SkVaDaR@users.noreply.github.com>
2021-06-30 19:34:00 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports.constants = require('./constant');
+module.exports.dataBaseTablesEnum = require('./database-tables.enum');
+module.exports.responseCodesEnum = require('./response-codes.enum');
+module.exports.responseMessages = require('./response-messages');
+module.exports.rolesEnum = require('./user-roles.enum');
