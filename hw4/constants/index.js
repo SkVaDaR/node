3f6ea7974b1959c4e5d96e73581a02b50a8a34c0
Sviatoslav Myskiv <79469781+SkVaDaR@users.noreply.github.com>
2021-06-30 18:54:12 +0300

    Add files via upload

--- hw4/constants/index.js
+++ hw4/constants/index.js
@@ -2,3 +2,4 @@ module.exports.constants = require('./constant');
 module.exports.dataBaseTablesEnum = require('./database-tables.enum');
 module.exports.responseCodesEnum = require('./response-codes.enum');
 module.exports.rolesEnum = require('./user-roles.enum');
+module.exports.responseMessages = require('./response-messages');
